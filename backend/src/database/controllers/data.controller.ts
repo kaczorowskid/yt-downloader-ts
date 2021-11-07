95e45c58424c942799f37f59947f782231462986
sijab <mar.jablonski96@wp.pl>
2021-11-07 13:33:53 +0100

    added data service

--- backend/src/database/controllers/data.controller.ts
+++ backend/src/database/controllers/data.controller.ts
@@ -1,45 +1,36 @@
 import { Request, Response } from 'express';
 import { Data } from '../models/Data';
+import { getAllDataService, deleteItemService, addItemService } from '../services/data.service';
 
 export const getAllData = async (req: Request, res: Response) => {
     const { id } = req.query;
 
-    try {
-        const data: any = await Data.findAll({
-            where: {
-                folder_id: id
-            },
-            attributes: ['id', 'url', 'thumbnail', 'title', 'duration'],
-        });
-        res.send(data);
-    } catch(e) {
-        console.log('getAllData error')
+    const data = await getAllDataService(id as string)
+
+    if(data) {
+        if(data.err!) res.json(data.e!)
+        else res.status(200).json(data.data!)
     }
 }
 
 export const deleteItem = async (req: Request, res: Response) => {
     const { id } = req.query;
 
-    try {
-        const data = await Data.destroy({where: {id: id}});
-        res.json({data: data})
-    } catch(e) {
-        console.log('deleteItem error')
+    const data = await deleteItemService(id as string)
+
+    if(data) {
+        if(data.err!) res.json(data.e!)
+        else res.status(200).json(data.data!)
     }
 }
 
 export const addItem = async (req: Request, res: Response) => {
-    const { folder_id, title, imageSrc, url } = req.query;
+    const { folder_id, title, imageSrc, url }: any = req.query;
 
-    try {
-        const data = await Data.create({
-            folder_id: Number(folder_id),
-            title: title as string,
-            thumbnail: imageSrc as string,
-            url: url as string
-        })
-        res.json({data: data})
-    } catch(e) {
-        console.log('addItem error');
+    const data = await addItemService(folder_id, title, imageSrc, url)
+
+    if(data) {
+        if(data.err!) res.json(data.e!)
+        else res.status(200).json(data.data!)
     }
 }
---
+++ backend/src/database/services/data.service.ts
@@ -0,0 +1,57 @@
+import { Data } from '../models/Data';
+
+export const getAllDataService = async (id: string) => {
+    try {
+        const data: any = await Data.findAll({
+            where: {
+                folder_id: id
+            },
+            attributes: ['id', 'url', 'thumbnail', 'title', 'duration'],
+        });
+        return {
+            err: false,
+            data: data
+        }
+    } catch(e) {
+        return {
+            err: true,
+            e
+        }
+    }
+}
+
+export const deleteItemService = async (id: string) => {
+    try {
+        const data = await Data.destroy({where: {id: id}});
+        return {
+            err: false,
+            data: data
+        }
+    } catch(e) {
+        return {
+            err: true,
+            e
+        }
+    }
+}
+
+export const addItemService = async (folder_id: string, title: string, imageSrc: string, url: string) => {
+
+    try {
+        const data = await Data.create({
+            folder_id: Number(folder_id),
+            title: title as string,
+            thumbnail: imageSrc as string,
+            url: url as string
+        })
+        return {
+            err: false,
+            data: data
+        }
+    } catch(e) {
+        return {
+            err: true,
+            e
+        }
+    }
+}
