ff97af0a5015f7231cf09751e160af4daa1ed8dd
sijab <mar.jablonski96@wp.pl>
2021-11-07 13:43:42 +0100

    added folder service

--- backend/src/database/controllers/folder.controller.ts
+++ backend/src/database/controllers/folder.controller.ts
@@ -1,38 +1,35 @@
 import { Request, Response } from 'express';
-import { Folders } from '../models/Folder';
+import { getFoldersService, deleteFolderService, addFolderService } from '../services/folder.service';
 
 export const getFolders = async (req: Request, res: Response) => {
     const { id } = req.query
 
-    try {
-        const data = await Folders.findAll({where: {user_id: id}, attributes: ['title', 'id']});
-        res.json({data: data, length: data.length});
-    } catch(e) {
-        console.log('getFolders error')
+    const data = await getFoldersService(id as string)
+
+    if (data) {
+        if (data.err!) res.json(data.e!)
+        else res.status(200).json({ data: data.data!, length: data.length! })
     }
 }
 
 export const deleteFolder = async (req: Request, res: Response) => {
     const { id, title } = req.query;
 
-    try {
-        const data = await Folders.destroy({where: {user_id: id, title: title}});
-        res.json({data: data})
-    } catch(e) {
-        console.log(e)
+    const data = await deleteFolderService(id as string, title as string)
+
+    if (data) {
+        if (data.err!) res.json(data.e!)
+        else res.status(200).json(data.data!)
     }
 }
 
 export const addFolder = async (req: Request, res: Response) => {
     const { id, title } = req.query;
- 
-    try {
-        const data = await Folders.create({
-            title: title as string, 
-            user_id: id as string
-        })
-        res.json({data: data})
-    } catch(e) {
-        console.log('addFolder error')
+
+    const data = await addFolderService(id as string, title as string)
+
+    if (data) {
+        if (data.err!) res.json(data.e!)
+        else res.status(200).json(data.data!)
     }
 }
---
+++ backend/src/database/services/folder.service.ts
@@ -0,0 +1,50 @@
+import { Folders } from '../models/Folder';
+
+export const getFoldersService = async (id: string) => {
+    try {
+        const data = await Folders.findAll({where: {user_id: id}, attributes: ['title', 'id']});
+        return {
+            err: false,
+            data: data,
+            length: data.length
+        }
+    } catch(e) {
+        return {
+            err: true,
+            e
+        }
+    }
+}
+
+export const deleteFolderService = async (id: string, title: string) => {
+    try {
+        const data = await Folders.destroy({where: {user_id: id, title: title}});
+        return {
+            err: false,
+            data: data
+        }
+    } catch(e) {
+        return {
+            err: true,
+            e
+        }
+    }
+}
+
+export const addFolderService = async (id: string, title: string) => {
+    try {
+        const data = await Folders.create({
+            title: title as string, 
+            user_id: id as string
+        })
+        return {
+            err: false,
+            data: data
+        }
+    } catch(e) {
+        return {
+            err: true,
+            e
+        }
+    }
+}
