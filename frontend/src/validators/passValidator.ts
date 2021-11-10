71e26ce23f04d7f94e9b4217628075add7b7c4ad
sijab <mar.jablonski96@wp.pl>
2021-11-10 23:12:26 +0100

    pass validate in front

--- frontend/src/validators/passValidator.ts
+++ frontend/src/validators/passValidator.ts
@@ -1,14 +1,8 @@
 const passwordRegexp = new RegExp('^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,}$');
 const emailRegexp = new RegExp('^\\S+@\\S+\.\\S+$');
 
-export const passwordValidate = (data: string) => passwordRegexp.test(data);
-export const emailValidate = (data: string) => emailRegexp.test(data);
-export const samePasswordValidate = (firstPassword: string, secondPassword: string) => firstPassword === secondPassword
-export const isEmpty = (data: string) => data === ''
-
 export const passValidator = {
     password: (data: string) => passwordRegexp.test(data),
     email: (data: string) => emailRegexp.test(data),
     samePassword: (firstPassword: string, secondPassword: string) => firstPassword === secondPassword,
-    isEmpty: (data: string) => data === ''
 };
