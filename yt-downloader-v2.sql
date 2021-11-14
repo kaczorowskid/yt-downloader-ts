8a1334572b76396ec438b11488bcfb76390db523
sijab <mar.jablonski96@wp.pl>
2021-11-14 12:29:46 +0100

    added example database

--- yt-downloader-v2.sql
+++ yt-downloader-v2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 14 Lis 2021, 12:04
+-- Czas generowania: 14 Lis 2021, 12:28
 -- Wersja serwera: 10.4.14-MariaDB
 -- Wersja PHP: 7.4.10
 
@@ -42,11 +42,12 @@ CREATE TABLE `data` (
 --
 
 INSERT INTO `data` (`id`, `folder_id`, `url`, `thumbnail`, `title`, `createdAt`, `updatedAt`) VALUES
-(102, 114, 'https://www.youtube.com/watch?v=m2uTFF_3MaA', 'https://i.ytimg.com/vi/m2uTFF_3MaA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqrxnHJ3VkhbHHHWjlHCWunKlKSg', 'The Beatles - Yellow Submarine', '2021-11-14', '2021-11-14'),
-(103, 114, 'https://www.youtube.com/watch?v=2Q_ZzBGPdqE', 'https://i.ytimg.com/vi/2Q_ZzBGPdqE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB0m7YsuX2y4jfGvVuez1_SJx6ruA', 'The Beatles - Help!', '2021-11-14', '2021-11-14'),
-(105, 112, 'https://www.youtube.com/watch?v=Z3w5gVM_4y8', 'https://i.ytimg.com/vi/Z3w5gVM_4y8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCEyyaAsHu1uYuPQWCLIbfeM8zdTA', 'Queen - I Want To Break Free (Soundtrack Mix)', '2021-11-14', '2021-11-14'),
-(106, 112, 'https://www.youtube.com/watch?v=-tJYN-eG1zk', 'https://i.ytimg.com/vi/-tJYN-eG1zk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLClUzRgg7Qr2DylZ8MGOdZbW_OBdA', 'Queen - We Will Rock You (Official Video)', '2021-11-14', '2021-11-14'),
-(107, 112, 'https://www.youtube.com/watch?v=HgzGwKwLmgM', 'https://i.ytimg.com/vi/HgzGwKwLmgM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAM8UqmFmnK5DyvGevPbHHZU_blAQ', 'Queen - Don\'t Stop Me Now (Official Video)', '2021-11-14', '2021-11-14');
+(110, 116, 'https://www.youtube.com/watch?v=Z3w5gVM_4y8', 'https://i.ytimg.com/vi/Z3w5gVM_4y8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCEyyaAsHu1uYuPQWCLIbfeM8zdTA', 'Queen - I Want To Break Free (Soundtrack Mix)', '2021-11-14', '2021-11-14'),
+(111, 116, 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ', 'https://i.ytimg.com/vi/fJ9rUzIMcZQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC7tkjMN8pT-zh4iBw5n9zg11OMFQ', 'Queen – Bohemian Rhapsody (Official Video Remastered)', '2021-11-14', '2021-11-14'),
+(112, 116, 'https://www.youtube.com/watch?v=HgzGwKwLmgM', 'https://i.ytimg.com/vi/HgzGwKwLmgM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAM8UqmFmnK5DyvGevPbHHZU_blAQ', 'Queen - Don\'t Stop Me Now (Official Video)', '2021-11-14', '2021-11-14'),
+(113, 117, 'https://www.youtube.com/watch?v=2Q_ZzBGPdqE', 'https://i.ytimg.com/vi/2Q_ZzBGPdqE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB0m7YsuX2y4jfGvVuez1_SJx6ruA', 'The Beatles - Help!', '2021-11-14', '2021-11-14'),
+(114, 117, 'https://www.youtube.com/watch?v=A_MjCqQoLLA', 'https://i.ytimg.com/vi/A_MjCqQoLLA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBiioAfwDsV_YhrHKc8tewZBzJr1g', 'The Beatles - Hey Jude', '2021-11-14', '2021-11-14'),
+(115, 117, 'https://www.youtube.com/watch?v=m2uTFF_3MaA', 'https://i.ytimg.com/vi/m2uTFF_3MaA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBqrxnHJ3VkhbHHHWjlHCWunKlKSg', 'The Beatles - Yellow Submarine', '2021-11-14', '2021-11-14');
 
 -- --------------------------------------------------------
 
@@ -67,8 +68,8 @@ CREATE TABLE `folders` (
 --
 
 INSERT INTO `folders` (`id`, `title`, `user_id`, `createdAt`, `updatedAt`) VALUES
-(112, 'queen', 32, '2021-11-14', '2021-11-14'),
-(114, 'The Beatles ', 32, '2021-11-14', '2021-11-14');
+(116, 'Queen', 35, '2021-11-14', '2021-11-14'),
+(117, 'The Beatles', 35, '2021-11-14', '2021-11-14');
 
 -- --------------------------------------------------------
 
@@ -90,7 +91,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `active`, `createdAt`, `updatedAt`) VALUES
-(32, 'admin@admin.pl', '$2b$10$tx9yH/g3YL9BPGAkyqEdIOlciljh4HULazHkdJuE57lAjCdaHwc3K', 1, '2021-11-14', '2021-11-14');
+(35, 'admin@admin.com', '$2b$10$9HrtF8guszA9tbXoyVn41u6O0NTucy2O0vT/fp9/Uyehf7JhZ8Jeu', 1, '2021-11-14', '2021-11-14');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -123,19 +124,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 
 --
 -- AUTO_INCREMENT dla tabeli `folders`
 --
 ALTER TABLE `folders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=115;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
